14cb9742d88cd998cc080df71a6d00ab6291ac07
roei <r.balish@gmail.com>
2025-02-04 16:26:08 +0200

    Added nav bar and footer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style/App.css'
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <p>שבוע טוב היום!</p>
+      <Navbar />
+      <div className="content">asasd</div>
+      <Footer className="Footer"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import '../style/Navbar.css'
+
+const Navbar = () => {
+    return ( 
+        <div className="nav">
+            <div className="buttonWrapper" >
+                <button>ברכה שבועית</button>
+                <button>עומוסקופ</button>
+                <button>רכילות</button>
+                <button>אודות</button>
+                <button>הרשמה לשירות</button>
+                <button>מדור שירה וחיברון</button>
+                <button>כוס תה</button>
+                <button>מעקב השבוע</button>
+                <button>פורום השבוע</button>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;

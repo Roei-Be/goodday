8fe07ba130701308649d046a1f78c171b166bd81
roei <r.balish@gmail.com>
2025-02-05 16:12:06 +0200

    Home page complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,21 @@ import { useContext, useState, } from 'react';
 import { DBContext } from '../context/SheetParsing';
 
 const Home = () => {
-    const [data, setData] = useState([]);
     const { bless } = useContext(DBContext);
 
     return (
         <div className="home">
-            <p>ברכה שבועית</p>
-            <p>{bless[0].bless}</p>
+            <div className="landing">
+                <div className="yellowstrip left"></div>
+                <div className="yellowstrip right"></div>
+                <div className="dashed"></div>
+                <div className='scroll'>⬇️גלול... הברכה מחכה לך⬇️</div>
+                <div className='scroll1'>עוד טיפה</div>
+                <div className="blessWrapper">
+                    <p>ברכה שבועית</p>
+                    <p>{bless && bless[0].bless}</p>
+                </div>
+            </div>
         </div>
     );
 }

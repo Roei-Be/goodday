d2adf36f84849c6b675fb45f70db2254e5455535
Roei <r.balisha@gmail.com>
2025-02-05 19:38:16 +0200

    Added naviation between pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
+import { BrowserRouter, Routes, Route, NavLink, useNavigate } from 'react-router-dom';
+
 function App() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="App">
       <p>
         שבוע טוב היום!
       </p>
-      <Navbar />
-      <Home />
+      <Navbar navigate={navigate}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<div>אודות</div>} />
+        <Route path="/contact" element={<div>צור קשר</div>} />
+      </Routes>
       <Footer className="Footer"/>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import '../style/Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({ navigate }) => {
     return ( 
         <div className="nav">
-            <div className="buttonWrapper" >
-                <button>ברכה שבועית</button>
-                <button>עומוסקופ</button>
-                <button>רכילות</button>
-                <button>אודות</button>
-                <button>הרשמה לשירות</button>
-                <button>מדור שירה וחיברון</button>
-                <button>כוס תה</button>
-                <button>מעקב השבוע</button>
-                <button>פורום השבוע</button>
+            <div className="divWrapper" >
+                <div onClick={() => navigate('/')}>ברכה שבועית</div>
+                <div onClick={() => navigate('/1')}>עומוסקופ</div>
+                <div onClick={() => navigate('/2')}>רכילות</div>
+                <div onClick={() => navigate('/4')}>אודות</div>
+                <div onClick={() => navigate('/')}>הרשמה לשירות</div>
+                <div onClick={() => navigate('/')}>מדור שירה וחיברון</div>
+                <div onClick={() => navigate('/')}>כוס תה</div>
+                <div onClick={() => navigate('/')}>מעקב השבוע</div>
+                <div onClick={() => navigate('/')}>פורום השבוע</div>
             </div>
         </div>
     );
